ffa9d0e9069b7ddd1f7ddacbfeae8cd11ff99f35
nockd61-sudo <nockd61@gmail.com>
2026-07-06 18:20:12 +0900

    Delete js directory

--- js/app.js
+++
@@ -1,5 +0,0 @@
-const grid=document.getElementById('worksGrid');
-const empty=document.getElementById('emptyState');
-// 다음 단계에서 Cloudflare Worker API에서 작품 목록을 불러옵니다.
-const artworks=[];
-if(artworks.length===0){empty.style.display='flex';}else{empty.style.display='none';}
