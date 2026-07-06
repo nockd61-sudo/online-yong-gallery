4de2f95c2a9b4da1079aff9c5e2aa8a313953060
nockd61-sudo <nockd61@gmail.com>
2026-07-06 19:45:43 +0900

    Update YONGRYEON_API_BASE to new subdomain

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
-// Worker 주소를 넣으세요. 예: https://yongryeon-api.계정명.workers.dev
-window.YONGRYEON_API_BASE = 'https://yongryeon-api.YOUR_SUBDOMAIN.workers.dev';
+window.YONGRYEON_API_BASE =
+'https://yongryeon-api.nockd61.workers.dev';
